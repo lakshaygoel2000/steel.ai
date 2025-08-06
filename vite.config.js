eccb78e5a1f54e2d2b064675c00db17f35428e89
laksh@DESKTOP-M5DG7M6 <lakshaygoel9650@gmail.com>
2025-08-06 13:55:28 +0530

    minor ui fixes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.mov']
+  assetsInclude: ['**/*.mov'],
+  server:{
+    host:true,
+    port:5173,
+  }
 });
